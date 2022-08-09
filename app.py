b3f0c94afda3d4a16af9860629f65e7b991ee128
MaysAb23-meet <109899447+MaysAb23-meet@users.noreply.github.com>
2022-08-09 12:20:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
+import json
 
 Config = {
   "apiKey": "AIzaSyDjObFBsV8oPDN_AXa9cjNPvlKM8QWqisg",
@@ -12,6 +13,41 @@ Config = {
   "appId": "1:533293150836:web:c35844f392d7972074d721",
   "measurementId": "G-0THL94HBP0" , "databaseURL": "https://final-project-817f0-default-rtdb.europe-west1.firebasedatabase.app/"
 };
+
+@app.route('/', methods=['GET', 'POST'])
+def signin():
+    error = ""
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       return redirect(url_for('add_tweet'))
+       try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('home'))
+       except:
+           error = "Authentication failed"
+
+    return render_template("signin.html")
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       full_name = request.form['full_name']
+       username = request.form['username']
+       bio = request.form['bio']
+
+       user = {"full_name": full_name, "username": username,"bio":bio,"email":email,"password":password}
+       try:
+            login_session['user'] =  auth.create_user_with_email_and_password(email, password)
+            db.child("Users").child(login_session['user']['localId']).set(user)
+            
+            return redirect(url_for('add_tweet'))
+       except:
+           error = "authentication failed"
+    return render_template("signup.html")
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
